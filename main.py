0e81f4e706ac71dd8ff6bbcb4d7465051d1970f0
JorgenBrattang <jorgen.kvist@gmail.com>
2022-08-24 06:11:09 +0000

    Add function check_if_exists(), checks if username exists already.

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('daily-math')
 
-
+# Sets up the variables
 users_worksheet = SHEET.worksheet("users")
+data = users_worksheet.get_all_values()
 
 
 def create_user_name():
     """ Creates new user name """
-    return input("Enter your name: ")
+    user_input = input("Enter your name: ")
+    return user_input.lower()
 
 
 def create_user_pin():
@@ -33,20 +35,30 @@ def update_users_worksheet(user_name, pin_code):
     google sheet with the user input from
     functions create_user_name() and create_user_pin()
     """
-    create_list = [user_name, pin_code]
-    users_worksheet.append_row(create_list)
+    # create_list = [user_name, pin_code]
+    check_if_exists(user_name)
+    # users_worksheet.append_row(create_list)
+
+
+def check_if_exists(user_name):
+    """
+    Checks the google sheet if the user already exists.
+    """
+    cell = users_worksheet.find(user_name, in_column=1)
+    if cell is not None:
+        print("Your selected username exists, please try another")
+    else:
+        print("Does not exists, continue to create pin number")
 
 
 def main():
     """
     Runs the programs functions.
     """
-    user_str = create_user_name()
-    pin_int = create_user_pin()
-    update_users_worksheet(user_str, pin_int)
-    data = users_worksheet.get_all_values()
-    print(data)
+    # user_str = create_user_name()
+    # pin_int = create_user_pin()
+    # update_users_worksheet(user_str, pin_int)
 
 
-main()
-
+# main()
+check_if_exists("not")
