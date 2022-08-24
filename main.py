2420a7a8f7cb3aabdc297ec73efd6438f23af28f
JorgenBrattang <jorgen.kvist@gmail.com>
2022-08-24 05:12:43 +0000

    Create create_user function to get user input for users login creds.

--- main.py
+++ main.py
@@ -13,8 +13,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('daily-math')
 
-users = SHEET.worksheet('users')
 
-data = users.get_all_values()
+def create_user():
+    """ Creates new user """
+    print("Create your login name: ")
+    user_str = input("Enter your name: ")
+    print(f"The chosen name is {user_str}\n")
 
-print(data)
+    print("Create your pin code: ")
+    pin_code = input("Enter your pin code: ")
+    print(f"The chosen pin code is {pin_code}")
+
+
+create_user()
